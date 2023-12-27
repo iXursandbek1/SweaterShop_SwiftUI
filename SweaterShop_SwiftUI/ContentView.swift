126d777d7f9b5283c06b49021afd378000e235bd
Xursandbek <xursandbek06082001@mail.com>
2023-12-27 15:12:52 +0500

    Initial Commit

---
+++ SweaterShop_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SweaterShop_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 27/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SweaterShop_SwiftUI/SweaterShop_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShop_SwiftUIApp.swift
+//  SweaterShop_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShop_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
